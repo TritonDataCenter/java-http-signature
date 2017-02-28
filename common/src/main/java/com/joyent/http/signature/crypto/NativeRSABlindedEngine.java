ec7f21bb0166c7a5b5675239b5db6cac4196a0f8
Elijah Zupancic <elijah@zupancic.name>
2017-02-28 12:37:42 -0800

    Added explicit headers for MIT and Apache 2.0 licensed classes.

--- common/src/main/java/com/joyent/http/signature/crypto/NativeRSABlindedEngine.java
+++ common/src/main/java/com/joyent/http/signature/crypto/NativeRSABlindedEngine.java
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2000 - 2016 The Legion of the Bouncy Castle Inc. (https://www.bouncycastle.org)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
 package com.joyent.http.signature.crypto;
 
 import com.squareup.jnagmp.Gmp;
@@ -17,7 +38,7 @@ import java.security.SecureRandom;
  * replace with a native implementation. We copied the library code here
  * because there is no better way to for us to inherit the properties.</p>
  *
- * <p>Note: there is a single change on line 118 - we use libgmp to do modpow.</p>
+ * <p>Note: changes from the original are only using libgmp to do modpow.</p>
  *
  * <p>Relevant copyright belongs to:<br>
  * Copyright (c) 2000 - 2015 The Legion of the Bouncy Castle Inc. (http://www.bouncycastle.org)
